d9aa8acf81bf44de1f7848d44a4f3b3dcda3145a
siddesh <siddusrb@gmail.com>
2020-01-26 14:23:03 +0530

    Spring boot project initialization

---
+++ src/main/java/org/sid/designpatterns/DesignPatternsPractice.java
@@ -0,0 +1,12 @@
+package org.sid.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsPractice {
+    public static void main(String[] args) {
+
+        SpringApplication.run(DesignPatternsPractice.class, args);
+    }
+}
